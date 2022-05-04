cadcf28c62880874ee7dc701bfdf2da9e477aba8
Marcos Vinicio Fonseca Barros <mfonsecabarros@gmail.com>
2022-05-03 21:19:01 -0300

    menu-toggle-end

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 
-
+let openMenu = document.querySelector('.open-menu');
+let closeMenu = document.querySelector('.close-menu');
 let nav = document.querySelector('#navigation');
+
+
+openMenu.addEventListener('click', ()=>{
+    document.body.setAttribute('class', 'menu-expanded')
+});
+
+closeMenu.addEventListener('click', ()=>{
+    document.body.removeAttribute('class', 'menu-expanded')
+});
+
 window.addEventListener('scroll', ()=>{
     window.scrollY > 0 ? nav.setAttribute('class','scroll') : nav.removeAttribute('class', 'scroll');
 });
