62ca1f2820eeeb50911914a6700b7520844da62f
Marcos Vinicio Fonseca Barros <mfonsecabarros@gmail.com>
2022-05-04 10:26:44 -0300

    forEach buttons navegation

--- main.js
+++ main.js
@@ -2,16 +2,24 @@
 let openMenu = document.querySelector('.open-menu');
 let closeMenu = document.querySelector('.close-menu');
 let nav = document.querySelector('#navigation');
+let btNavegationMenu = document.querySelectorAll('.buttonNavigation a');
+
+
+function btCloseMenu() {
+    document.body.removeAttribute('class', 'menu-expanded')
+};
 
 
 openMenu.addEventListener('click', ()=>{
     document.body.setAttribute('class', 'menu-expanded')
 });
 
-closeMenu.addEventListener('click', ()=>{
-    document.body.removeAttribute('class', 'menu-expanded')
-});
+closeMenu.addEventListener('click', btCloseMenu);
 
 window.addEventListener('scroll', ()=>{
     window.scrollY > 0 ? nav.setAttribute('class','scroll') : nav.removeAttribute('class', 'scroll');
 });
+
+btNavegationMenu.forEach(item=>{
+    item.addEventListener('click', btCloseMenu);
+})
