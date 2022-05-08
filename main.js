54b8c831111a74dbdc41cf678bb3d55e93eef707
Marcos Vinicio Fonseca Barros <mfonsecabarros@gmail.com>
2022-05-08 11:35:35 -0300

    Moviment itens menu

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
 let openMenu = document.querySelector('.open-menu');
 let closeMenu = document.querySelector('.close-menu');
 let nav = document.querySelector('#navigation');
+let home = document.querySelector('body');
 let btNavegationMenu = document.querySelectorAll('.buttonNavigation a');
 let backToTop = document.querySelector('#backToTopButton');
 
 function btCloseMenu() {
     document.body.removeAttribute('class', 'menu-expanded')
 };
+
+function activateMenuAtCurrentSection() {
+    const targetLine = scrollY + (innerHeight/2);
+
+    if(targetLine <= 1130) {
+        btNavegationMenu[0].setAttribute('class', 'active');
+        btNavegationMenu[1].removeAttribute('class', 'active');
+        btNavegationMenu[2].removeAttribute('class', 'active');
+    } else if (targetLine < 2123 && targetLine >1130){
+        btNavegationMenu[1].setAttribute('class', 'active');
+        btNavegationMenu[2].removeAttribute('class', 'active');
+        btNavegationMenu[0].removeAttribute('class', 'active');
+    } else {
+        btNavegationMenu[2].setAttribute('class', 'active');
+        btNavegationMenu[0].removeAttribute('class', 'active');
+        btNavegationMenu[1].removeAttribute('class', 'active');
+    }
+    
+    
+}
+
+
 
 openMenu.addEventListener('click', ()=>{
     document.body.setAttribute('class', 'menu-expanded')
@@ -17,7 +40,8 @@ closeMenu.addEventListener('click', btCloseMenu);
 window.addEventListener('scroll', ()=>{
     showNavOnScroll();
     showButtonOnScroll();
-});
+    activateMenuAtCurrentSection();
+})
 
 function showNavOnScroll() {
     window.scrollY > 0 ? nav.setAttribute('class','scroll') : nav.removeAttribute('class', 'scroll');
